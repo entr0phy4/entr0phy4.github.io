83809cda497d2bf850a6236a944801a7a57d687b
David A. <davidagamez04@gmail.com>
2025-05-01 16:49:57 +0000

    init: blog base

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import { type Metadata } from 'next'
+
+import { RootLayout } from '@/components/RootLayout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Entr0phy4',
+    default: 'Studio - Award winning developer studio based in Denmark',
+  },
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className="h-full text-base antialiased">
+      <body className="flex min-h-full flex-col">
+        <RootLayout>{children}</RootLayout>
+      </body>
+    </html>
+  )
+}
