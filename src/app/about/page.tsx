60cbd4649baadc2227b6c21a64e243757ca0821c
David A. <davidagamez04@gmail.com>
2025-05-29 21:10:49 +0000

    refactor: clean up About page by removing unused team member data and related imports

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,28 +2,12 @@ import { type Metadata } from 'next'
 import Image from 'next/image'
 
 import { Border } from '@/components/Border'
-import { ContactSection } from '@/components/ContactSection'
 import { Container } from '@/components/Container'
 import { FadeIn, FadeInStagger } from '@/components/FadeIn'
 import { GridList, GridListItem } from '@/components/GridList'
 import { PageIntro } from '@/components/PageIntro'
-import { PageLinks } from '@/components/PageLinks'
 import { SectionIntro } from '@/components/SectionIntro'
-import { StatList, StatListItem } from '@/components/StatList'
-import imageAngelaFisher from '@/images/team/angela-fisher.jpg'
-import imageBenjaminRussel from '@/images/team/benjamin-russel.jpg'
-import imageBlakeReid from '@/images/team/blake-reid.jpg'
-import imageChelseaHagon from '@/images/team/chelsea-hagon.jpg'
-import imageDriesVincent from '@/images/team/dries-vincent.jpg'
-import imageEmmaDorsey from '@/images/team/emma-dorsey.jpg'
-import imageJeffreyWebb from '@/images/team/jeffrey-webb.jpg'
-import imageKathrynMurphy from '@/images/team/kathryn-murphy.jpg'
-import imageLeonardKrasner from '@/images/team/leonard-krasner.jpg'
-import imageLeslieAlexander from '@/images/team/leslie-alexander.jpg'
-import imageMichaelFoster from '@/images/team/michael-foster.jpg'
-import imageWhitneyFrancis from '@/images/team/whitney-francis.jpg'
 import { loadArticles } from '@/lib/mdx'
-import { Button } from '@/components/Button'
 
 function Culture() {
   return (
@@ -56,127 +40,78 @@ function Culture() {
   )
 }
 
-const team = [
-  {
-    title: 'Leadership',
-    people: [
-      {
-        name: 'Leslie Alexander',
-        role: 'Co-Founder / CEO',
-        image: { src: imageLeslieAlexander },
-      },
-      {
-        name: 'Michael Foster',
-        role: 'Co-Founder / CTO',
-        image: { src: imageMichaelFoster },
-      },
-      {
-        name: 'Dries Vincent',
-        role: 'Partner & Business Relations',
-        image: { src: imageDriesVincent },
-      },
-    ],
-  },
-  {
-    title: 'Team',
-    people: [
-      {
-        name: 'Chelsea Hagon',
-        role: 'Senior Developer',
-        image: { src: imageChelseaHagon },
-      },
-      {
-        name: 'Emma Dorsey',
-        role: 'Senior Designer',
-        image: { src: imageEmmaDorsey },
-      },
-      {
-        name: 'Leonard Krasner',
-        role: 'VP, User Experience',
-        image: { src: imageLeonardKrasner },
-      },
-      {
-        name: 'Blake Reid',
-        role: 'Junior Copywriter',
-        image: { src: imageBlakeReid },
-      },
-      {
-        name: 'Kathryn Murphy',
-        role: 'VP, Human Resources',
-        image: { src: imageKathrynMurphy },
-      },
-      {
-        name: 'Whitney Francis',
-        role: 'Content Specialist',
-        image: { src: imageWhitneyFrancis },
-      },
-      {
-        name: 'Jeffrey Webb',
-        role: 'Account Coordinator',
-        image: { src: imageJeffreyWebb },
-      },
-      {
-        name: 'Benjamin Russel',
-        role: 'Senior Developer',
-        image: { src: imageBenjaminRussel },
-      },
-      {
-        name: 'Angela Fisher',
-        role: 'Front-end Developer',
-        image: { src: imageAngelaFisher },
-      },
-    ],
-  },
-]
-
-function Team() {
-  return (
-    <Container className="mt-24 sm:mt-32 lg:mt-40">
-      <div className="space-y-24">
-        {team.map((group) => (
-          <FadeInStagger key={group.title}>
-            <Border as={FadeIn} />
-            <div className="grid grid-cols-1 gap-6 pt-12 sm:pt-16 lg:grid-cols-4 xl:gap-8">
-              <FadeIn>
-                <h2 className="font-display text-2xl font-semibold text-neutral-950">
-                  {group.title}
-                </h2>
-              </FadeIn>
-              <div className="lg:col-span-3">
-                <ul
-                  role="list"
-                  className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-8"
-                >
-                  {group.people.map((person) => (
-                    <li key={person.name}>
-                      <FadeIn>
-                        <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
-                          <Image
-                            alt=""
-                            {...person.image}
-                            className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
-                          />
-                          <div className="absolute inset-0 flex flex-col justify-end bg-linear-to-t from-black to-black/0 to-40% p-6">
-                            <p className="font-display text-base/6 font-semibold tracking-wide text-white">
-                              {person.name}
-                            </p>
-                            <p className="mt-2 text-sm text-white">
-                              {person.role}
-                            </p>
-                          </div>
-                        </div>
-                      </FadeIn>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </FadeInStagger>
-        ))}
-      </div>
-    </Container>
-  )
-}
+// const team = [
+//   {
+//     title: 'Leadership',
+//     people: [
+//       {
+//         name: 'Leslie Alexander',
+//         role: 'Co-Founder / CEO',
+//         image: { src: imageLeslieAlexander },
+//       },
+//       {
+//         name: 'Michael Foster',
+//         role: 'Co-Founder / CTO',
+//         image: { src: imageMichaelFoster },
+//       },
+//       {
+//         name: 'Dries Vincent',
+//         role: 'Partner & Business Relations',
+//         image: { src: imageDriesVincent },
+//       },
+//     ],
+//   },
+//   {
+//     title: 'Team',
+//     people: [
+//       {
+//         name: 'Chelsea Hagon',
+//         role: 'Senior Developer',
+//         image: { src: imageChelseaHagon },
+//       },
+//       {
+//         name: 'Emma Dorsey',
+//         role: 'Senior Designer',
+//         image: { src: imageEmmaDorsey },
+//       },
+//       {
+//         name: 'Leonard Krasner',
+//         role: 'VP, User Experience',
+//         image: { src: imageLeonardKrasner },
+//       },
+//       {
+//         name: 'Blake Reid',
+//         role: 'Junior Copywriter',
+//         image: { src: imageBlakeReid },
+//       },
+//       {
+//         name: 'Kathryn Murphy',
+//         role: 'VP, Human Resources',
+//         image: { src: imageKathrynMurphy },
+//       },
+//       {
+//         name: 'Whitney Francis',
+//         role: 'Content Specialist',
+//         image: { src: imageWhitneyFrancis },
+//       },
+//       {
+//         name: 'Jeffrey Webb',
+//         role: 'Account Coordinator',
+//         image: { src: imageJeffreyWebb },
+//       },
+//       {
+//         name: 'Benjamin Russel',
+//         role: 'Senior Developer',
+//         image: { src: imageBenjaminRussel },
+//       },
+//       {
+//         name: 'Angela Fisher',
+//         role: 'Front-end Developer',
+//         image: { src: imageAngelaFisher },
+//       },
+//     ],
+//   },
+// ]
 
 export const metadata: Metadata = {
   title: 'About Us',
